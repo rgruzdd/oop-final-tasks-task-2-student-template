7882ef8a56844f7e47657bc7750aa99515eca73d
Дмитрий Колб <dm.kolb@gmail.com>
2022-09-01 08:13:56 +0000

    Update tasks/task.py

--- tasks/task.py
+++ tasks/task.py
@@ -1,6 +1,8 @@
 class Cipher:
     def encode(self, data):
         pass
+        #TODO: please add your code here
     
     def decode(self, data):
         pass
+         #TODO: please add your code here
